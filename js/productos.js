d7255f70311f4fea71e97dfd86b00b67a6326308
jessica mier <jessi.m@live.com.ar>
2024-05-23 20:59:50 -0300

    Se agrega javascript para las card dinamicas y click en botones

---
+++ js/productos.js
@@ -0,0 +1,174 @@
+const productosRon = [
+    {
+        id: 1,
+        titulo: "Havana Especial",
+        imagen: "img/havana.png",
+        descripcion: "Havana Club Especial es un ron dorado premium caracterizado por su sabor final suave. Perfectamente equilibrado, combina la ligereza del sabor asociada a los rones blancos con la complejidad afrutada de los rones más añejados.",
+        categoria: {
+            nombre: "Ron",
+            id: "ron"
+        },
+        precio: 15000
+    },
+    {
+        id: 2,
+        titulo: "Bacardi",
+        imagen: "img/bacardi.png",
+        descripcion: "Ron Bacardi Carta Blanco: Don Facundo fue el primero en envejecer intencionadamente el ron para conseguir un desarrollo específico del aroma y el sabor, envejeciendo el ron un mínimo de doce meses en barriles de roble blanco americano ligeramente carbonizado y que ofrecían al Ron BACARDÍ un grado de maduración óptimo.Hoy el ron blanco BACARDÍ Superior mantiene la excelencia de aquel primer ron creado 1862, cuyo complejo proceso de elaboración lo convierte en compañero perfecto para maridaje o en un solista de lujo.",
+        categoria: {
+            nombre: "Ron",
+            id: "ron"
+        },
+        precio: 15.400
+    },
+    {
+        id: 3,
+        titulo: "Barceló",
+        imagen: "img/barcelo.png",
+        descripcion: "El Barcelo Gran Añejo es una mezcla de rones añejados durante seis años o más en roble americano. Barceló Gran Añejo presenta aromas  de frutas cítricas y roble. Envejecido durante 6 años en barricas de bourbon, pero no filtrada con carbón. Se distinguen en la nariz aromas de vainilla, mantequilla, cáscara escarchada y aguardiente de cerezas que excitan los sentidos. En boca es dulce, pero aún más seco que el ron miel. La dulzura es relativamente breve, y se convierte en un sabor más complejo y coriáceo, con ligeras notas de jerez. El paso de boca es más impresionante, incluyen algunos tonos fruto picante y elementos de nuez, que llevan a la conclusión de que este ron se mezcla bien, y también proporciona su propio carácter.",
+        categoria: {
+            nombre: "Ron",
+            id: "ron"
+        },
+        precio: 26.700
+    },
+    {
+        id: 4,
+        titulo: "Diplomatico Reserva Exclusiva",
+        imagen: "img/diplomatico.png",
+        descripcion: "El Ron Diplomatico Reserva Exclusiva, es un ron complejo y elegante, cuidadosamente elaborado a partir de mieles de cañas de azúcar. Destilado en antiguos alambiques de cobre y envejecido en pequeñas barricas de roble. Es una mezcla de los mejores rones añejados hasta 12 años cuidadosamente seleccionados por el Maestro de la destilería. Su cuerpo único y excelente equilibrio, hacen de este galardonado ron, una referencia para los amantes y los conocedores de ron de todo el mundo. En nariz abre con aromas de cáscara de naranja, caramelo y regaliz; en paladar comienza suave y continua con notas dulces de caramelo, ofreciendo un final seductor y duradero.",
+        categoria: {
+            nombre: "Ron",
+            id: "ron"
+        },
+        precio: 113.500
+    },
+    {
+        id: 5,
+        titulo: "Santa Teresa",
+        imagen: "img/santateresa.png",
+        descripcion: "La Hacienda Santa Teresa es la productora más antigua de rones de Venezuela y sus orígenes se remontan a 1796 cuando el conde de Tovar y Blanco la fundó en los fértiles valles de Aragua. Un siglo más tarde esta fue adquirida por el descendiente alemán Gustavo Julio Vollmer, quien registró por primera vez la marca en 1909. La hacienda, lugar desde donde el libertador Simón Bolívar reiteró su proclama contra la esclavitud en 1818, está ubicada en el centro del país, en uno de sus valles más fértiles, de abundantes lluvias y temperaturas frescas nocturnas. Santa Teresa Claro es un ron de Venezuela con un ligero añejamiento. Asimismo, posee un cuerpo medio y delicada dulzura. También es un excelente ron para todo uso, ideal para fiestas o reuniones de verano.",
+        categoria: {
+            nombre: "Ron",
+            id: "ron"
+        },
+        precio: 28200
+    },
+    {
+        id: 6,
+        titulo: "Santiago de Cuba Extra Añejo Gran Reserva 20 Años",
+        imagen: "img/santiagodecuba.png",
+        descripcion: "Ron Santiago de Cuba Extra Añejo Gran Reserva 20 años es un Ron para aquellos que desean disfrutar el sabor rico de la vida. Con 20 años de envejecimiento en barricas de roble blanco americano, este Ron ofrece un color ámbar oscuro, aromas intensos a caramelo, frutos secos y vainilla, con notas de café y tabaco. En el paladar es muy suave y redondo, con un final largo y elegante. Perfecto para beber sin prisa mientras disfrutas de una buena conversación con amigos, este Ron es ideal para aquellos momentos de calma y tranquilidad.",
+        categoria: {
+            nombre: "Ron",
+            id: "ron"
+        },
+        precio: 256.500
+    }
+]
+
+const productosGin=[
+    {
+        id: 100,
+        titulo: "Beefeater",
+        imagen: "img/beefeater.jpg",
+        descripcion: "Esta ginebra, una London Dry Gin por antonomasia hecha con un intenso toque de enebro y fuertes notas cítricas, es una auténtica London Dry para quienes disfrutan del sabor auténtico de la ginebra. Con una tradición en el mundo de la destilación de 200 años, había una época en la que pesábamos a mano los extractos naturales de cada lote de ginebra que producíamos… Era hace dos días.",
+        categoria: {
+            nombre: "Gin",
+            id: "gin"
+        },
+        precio: 21000
+    },
+    {
+        id: 101,
+        titulo: "Bombay",
+        imagen: "img/bombay-750-.png",
+        descripcion: "Elaborado según una receta secreta que data de 1761, bombay sapphire se produce logrando una combinación única de diez ingredientes botánicos naturales seleccionados y un proceso exclusivo de destilación. El resultado es un complejo líquido aromático, con sabor refinado y un delicado equilibrio.",
+        categoria: {
+            nombre: "Gin",
+            id: "gin"
+        },
+        precio: 27000
+    },
+    {
+        id: 102,
+        titulo: "Bulldog",
+        imagen: "img/bulldog.png",
+        descripcion: "El aguardiente neutro de grano (96% ABV) se infunde con los sabores y aromas naturales de los 12 ingredientes botánicos característicos de BULLDOG Gin en un alambique de cobre. Esto permite que los ingredientes de la ginebra se maceren en el aguardiente antes de la destilación, para liberar sus aceites esenciales, dando lugar a la creación de vapor.",
+        categoria: {
+            nombre: "Gin",
+            id: "gin"
+        },
+        precio: 30000
+    },
+    {
+        id: 103,
+        titulo: "Heraclito & Macedonio",
+        imagen: "img/heraclito.png",
+        descripcion: "Su composición cuenta con 12 Botánicos de dominante enebro y notas florales, que aparte de aportarle un color rubí, único en su categoría, armoniza las notas de cardamomo y coriandro que se desarrollan en esta bebida, luego de una salida compleja de cítricos que se balancean en esta sutil formula con un leve picante que evoca una postal de la India. Cada botella es ÚNICA. Cada una de las botellas de Heráclito & Macedonio cuenta con sellos distintivos que las hacen irrepetibles.",
+        categoria: {
+            nombre: "Gin",
+            id: "gin"
+        },
+        precio: 11500
+    },
+    {
+        id: 104,
+        titulo: "Principe de los Apostoles",
+        imagen: "img/principe.png",
+        descripcion: "No vamos a contarte a qué huele o cómo entra en boca, Principe es una experiencia personal. Te invitamos a preparar un buen trago, o tomarlo solo refrescado y a viajar con El. Destilado en partidas de 200 litros de un alambique de cobre es tan artesanal como puede serlo.",
+        categoria: {
+            nombre: "Gin",
+            id: "gin"
+        },
+        precio: 8000
+    },
+    {
+        id: 105,
+        titulo: "Brighton",
+        imagen: "img/brighton.png",
+        descripcion: "Inspirado en una ciudad portuaria, donde la brisa de mar o la frescura de una noche cerca del río, te transporta a ese momento de escapada con amigos en primavera o en otoño, refrescando tu espíritu. Un gin de perfil clásico London Dry, con un diseño moderno y un precio accesible.",
+        categoria: {
+            nombre: "Gin",
+            id: "gin"
+        },
+        precio: 6500
+    }
+      
+]
+
+const sectionTarjetas = document.querySelector('.tarjetas');
+
+// Función para manejar el evento de clic en los botones "Comprar"
+function botonesClick(event) {
+    const productoId = event.target.id;
+    // Aquí puedes agregar la lógica para manejar la compra del producto
+    console.log(`Producto con ID ${productoId} agregado al carrito`);
+}
+
+// Función para cargar los productos
+function cargarProductos(arrayproductos){
+    sectionTarjetas.innerHTML= " " ;// vacia el contenedor
+    arrayproductos.forEach(producto => {
+        const div = document.createElement('div'); //por cada elemento del array crea un div
+        div.classList.add('card')//le agrega la clase card
+        //carga el contenedor con los datos provenientes del array
+        div.innerHTML = ` 
+        <img src="../${producto.imagen}" alt="Imagen de ejemplo" class="card-img">
+        <div class="card-content">
+            <h2 class="card-title">${producto.titulo}</h2>
+            <p class="card-description">${producto.descripcion}</p>
+            <button class="card-button" id="${producto.id}">Comprar</button>
+            <h3>$${producto.precio}</h3>
+        </div>
+        `
+        sectionTarjetas.append(div)//agrega el elemento al html
+    });
+
+    // Añadir eventos de clic a los botones "Comprar"
+    const botonesComprar = document.querySelectorAll('.card-button');
+    botonesComprar.forEach(boton => {
+        boton.addEventListener('click', botonesClick);
+    });
+}
+
